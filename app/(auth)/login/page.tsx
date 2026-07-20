98b9feb9cf972362f16cb609c9a1edfc408daf44
Nich1808 <samsreynich0508@gmail.com>
2026-07-20 20:01:31 +0700

    create login page

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,10 @@
+import { LoginForm } from "@/components/login-form";
+
+
+export default function SignupPage() {
+  return (
+    <main className="w-full max-w-5xl mx-auto p-4">
+      <LoginForm/>
+    </main>
+  );
+}
